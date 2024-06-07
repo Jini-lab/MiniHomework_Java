4a6fd9111dd8d6c807596a51591e49fcfe1b348b
Chung yeojin <84843910+Jini-lab@users.noreply.github.com>
2024-06-07 20:43:08 +0900

    Create JavaStudy01.java
    Printing the multiplication table on the console screen using the ‘format’ function

---
+++ Week1/JavaStudy01.java
@@ -0,0 +1,15 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Main {
+    public static void main(String[] args) {
+        System.out.println("[구구단 출력]");
+        for(int i=1; i<10; i++){
+            for(int j=1; j<10; j++){
+                System.out.print(String.format("%02d",j)+ " × " +String.format("%02d",i) +" = "+ String.format("%02d",i*j)+"\t");
+            }
+            System.out.println("");
+        }
+    }
+}
